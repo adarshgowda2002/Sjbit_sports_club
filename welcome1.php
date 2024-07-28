06169297f3ee9ad661f55b6a730b9ca9fff1fbbf
Adarsh.k <102061147+adarshgowda2002@users.noreply.github.com>
2024-07-28 11:47:51 +0530

    Add files via upload
    XAMPP v3.3.0
    PHP
     MySQL  Database
    HTML
    CSS
    JavaScript
    Ajax
    jQuery
    Bootstrap
    Font-awesome
    Bootstrap DataTables
    AdminLTE Template
    This Simple Online Sports Complex Booking System can be accessed by the Management Admin or Staff, Registered or Possible Clients, and to the Public. This project has an Admin Panel where the management manages the data in the system such as the list of the facilities. The Admin Panel can be accessed by 2 different user roles which are the Admin and the Staff. The Admin Users have the privilege to access all the features and functionalities on this side while the Staff User has only limited. The possible client can simply register their own account in order them to gain access to the booking feature of the system. Clients can book the facilities they wanted on their desired date as long as the dates do not complicate with the other bookings. The client-side has a booking list where their new and previously booked facilities are listed with details. In this system, Anonymous Visitors can explore the facilities and information about the system of the business.
    
    Features
    Management-Side
    Secure Login and Logout
    Dashboard
    Category Management
    Add new Category
    List All Categories
    Update Category Details
    Delete Category Details
    Facility Management
    Add new Facility
    List All Facilities
    Update Facility Details
    Delete Facility Details
    Booking Management
    List All Bookings
    View Booking Details
    Confirm Booking Details
    Mark Booking as Done Details
    Cancel Booking Details
    Delete Booking Details
    Client Management
    List All Clients
    Update Client Details
    Delete Client Details
    System User Management
    Add new System User
    List All System Users
    Update System User Details
    Delete System User Details
    System Information Management
    Client-Side
    Secure Login and Registration
    Home Page Content
    About Content
    View All Facilities
    Search Facility
    View Facility Details
    Book Facility
    List Bookings
    View Booking Details
    Cancel Pending Booking
    Manage/Update Account Information/Credentials
    Logout
    Public-Side
    Home Page Content
    About Content
    View All Facilities
    Search Facility
    View Facility Details
    The source code was developed only for educational purposes only. You can download the source code for free and modify it the way you wanted.
    
    System Snapshots of some Features
    Facility Page (Public)
    Online Sports Complex Booking System
    Facility Details (Public)
    Online Sports Complex Booking System
    Client Booking List
    Online Sports Complex Booking System
    Booked Facility Details Modal (Client)
    Online Sports Complex Booking System
    Admin Panel
    Online Sports Complex Booking System
    Download and Install any local web server such as XAMPP/WAMP.
    Download the provided source code zip file. (download button is located below)
    Installation/Setup
    
    Open your XAMPP/WAMP's php.ini file and Enable/Uncomment the GD Library.
    Open your XAMPP/WAMP's Control Panel and start Apache and MySQL.
    Extract the downloaded source code zip file.
    If you are using XAMPP, copy the extracted source code folder and paste it into the XAMPP's "htdocs" directory. And If you are using WAMP, paste it into the "www" directory.
    Browse the PHPMyAdmin in a browser. i.e. http://localhost/phpmyadmin
    Create a new  database naming scbs_db.
    Import the provided SQL file. The file is known as scbs_db.sql located inside the database folder.
    Browse the Online Sports Complex Booking System in a browser. i.e. http://localhost/scbs/.
    Default Admin Access
    Username: admin
    Password: admin123

---
+++ welcome1.php
@@ -0,0 +1,53 @@
+<html>
+<head>
+<title> mini project</title>
+<style>
+body {
+  background-image: url("stdium.jpg");
+background-repeat: no-repeat, repeat;
+background-size: cover;
+}
+.button {
+  border: none;
+  color: #b3ffd9";
+  padding: 16px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  transition-duration: 0.4s;
+  cursor: pointer;
+}
+
+.button1 {
+  background-color: white; 
+  color: black; 
+  border: 2px solid #04AA6D;
+}
+
+.button1:hover {
+  background-color: #04AA6D;
+  color: white;
+}
+</style>
+<body bgcolor="#b3ffd9">
+
+
+<center>
+<image src="logo.jpg" height="100" width="150">
+
+<h4><font color="#121254"> ||JAI SRI GURUDEV||</font></H4>
+<h3><font color="#ccffee">Sri Adhichunchanagiri Shikshana Trust</h3></font>
+<h1><font color="white">SJB INSTITUTE OF TECHNOLOGY </font></h1>
+<h3><font color="#000033">(A Constituent Institution of BGS & SJB Group of Institution & Hospitals)</font></h3>
+<h1><font color=" #ffff99"> Welcome to SJBIT Sports Corner</font></h1>
+<a href="index.php">
+<button class="button button1">Click here to know more!! </button></a>
+
+</center>
+
+</h1>
+</body>
+
+</html>
